c2139ec1be29fc8f2a37374cca1a4cceafe2ddbc
Mimozë Lladrovci <mimoze.lladrovci@sparebank1.no>
2022-12-09 22:42:14 +0100

    Adding app.js and Dockerfile

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
+
+module.exports = app;
